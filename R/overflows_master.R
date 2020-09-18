ff3d5626014536c5e093873cbe60fc87a9af15f4
fiona-grimm <Fiona.grimm@health.org.uk>
2020-09-18 09:26:21 +0100

    Increase time series forecast window to 78 weeks, add LoS analysis

--- R/overflows_master.R
+++ R/overflows_master.R
@@ -181,14 +181,13 @@ time_series_data[[4]] <- time_series_data[[4]] %>%
 
 times_series_forecasts <- running_forecasts(total_cohort_data = time_series_data,
                                             train_date = as.Date("2020-03-02"),
-                                            forecast_period = 53,
+                                            forecast_period = 78,
                                             base_dir = "time_series_forecasts/",
                                             run_admis = TRUE, forecast_admis = TRUE,
                                             forecast_WT = TRUE, forecast_frail = TRUE,
                                             cutoff_dates = cutoff_dates)
 
 saveRDS(times_series_forecasts, here::here("time_series_forecasts", "time_series_forecasts.Rds"))
-
 
 
 # -------------------------------------------------------------------------
@@ -333,3 +332,93 @@ calculate_unitcosts(full_mergedcosts,
                     costs_directory = "../Reference costs/", 
                     output_dir = here::here("costing_results"))
   
+
+
+# Length of stay frequency tables -----------------------------------------
+
+source("src/los_freq_tables.R")
+
+# Take only 1 year of data (11-12 in this case)
+los_data <- select(transitions_data, c("GA_LoS", "cc_LoS", "admimeth_C", "ICD",
+                               "agegrp_v3", "cc_start_flg","cohort","Total_LoS"))
+
+## discard negative values LoS and limit to <= 75
+los_data <- subset(los_data, Total_LoS >= 0 & Total_LoS <= 75)
+
+los_data$disease <- "ICD"
+los_data$age <- paste("_AGE", los_data$agegrp_v3, sep = "")
+
+los_data$ptgrp <- paste0(los_data$disease, los_data$ICD, los_data$age)
+los_data$a <- ifelse(los_data$admimeth_C == 1, "N", "E")
+
+# Identify patients in Elective vs. Emergency
+elective <- subset(los_data, admimeth_C == 1)
+emergency <- subset(los_data, admimeth_C == 2)
+
+# Identify patients who start in GA vs. CC and Elective vs. Emergency
+N_ga <- subset(los_data, admimeth_C == 1 & cc_start_flg == 0)
+E_ga <- subset(los_data, admimeth_C == 2 & cc_start_flg == 0)
+
+N_cc <- subset(los_data, admimeth_C == 1 & cc_start_flg == 1)
+E_cc <- subset(los_data, admimeth_C == 2 & cc_start_flg == 1)
+
+# frequency and proportion tables for each category
+# All electives
+count_elective <- as.data.frame.matrix(table(elective$Total_LoS, elective$ptgrp))
+aggregated_dfs_elec <- aggregate_weekly(count_elective)
+
+# All emergencies
+count_emergency <- as.data.frame.matrix(table(emergency$Total_LoS, emergency$ptgrp))
+aggregated_dfs_emerg <- aggregate_weekly(count_emergency)
+
+# Elective patients starting in G&A
+count_N_ga <- as.data.frame.matrix(table(N_ga$Total_LoS, N_ga$ptgrp))
+aggregated_dfs_elec_GA <- aggregate_weekly(count_N_ga)
+
+# Emergency patients starting in G&A
+count_E_ga <- as.data.frame.matrix(table(E_ga$Total_LoS, E_ga$ptgrp))
+aggregated_dfs_emerg_GA <- aggregate_weekly(count_E_ga)
+
+# Elective patients starting in CC
+count_N_cc <- as.data.frame.matrix(table(N_cc$Total_LoS, N_cc$ptgrp))
+aggregated_dfs_elec_CC <- aggregate_weekly(count_N_cc)
+
+# Emergency patients starting in CC
+count_E_cc <- as.data.frame.matrix(table(E_cc$Total_LoS, E_cc$ptgrp))
+aggregated_dfs_emerg_CC <- aggregate_weekly(count_E_cc)
+
+
+## Simplify counts to remove those under 10 ###################################
+
+## all electives 
+cut_off_elec <- count_threshold(aggregated_dfs_elec, cut_off = 10)
+
+## All emergencies 
+cut_off_emerg <- count_threshold(aggregated_dfs_emerg, cut_off = 10)
+
+## Electives starting in G&A 
+cut_off_elec_GA <- count_threshold(aggregated_dfs_elec_GA, cut_off = 10)
+
+## Emergencies starting in G&A
+cut_off_emerg_GA <- count_threshold(aggregated_dfs_emerg_GA, cut_off = 10)
+
+## Electives starting in CC
+cut_off_elec_CC <- count_threshold(aggregated_dfs_elec_CC, cut_off = 10)
+
+## Emergencies starting in CC
+cut_off_emerg_CC <- count_threshold(aggregated_dfs_emerg_CC, cut_off = 10)
+
+
+df_list <- list("all_emergency_count" = cut_off_emerg[[1]], "all_emergency_prop" = cut_off_emerg[[2]],
+                "emergency_ga_count" = cut_off_emerg_GA[[1]] ,"emergency_ga_prop" = cut_off_emerg_GA[[2]],
+                "emergency_cc_count" = cut_off_emerg_CC[[1]], "emergency_cc_prop" = cut_off_emerg_CC[[2]],
+                "all_elective_count" = cut_off_elec[[1]], "all_elective_prop" = cut_off_elec[[2]],
+                "elective_ga_count" = cut_off_elec_GA[[1]], "elective_ga_prop" = cut_off_elec_GA[[2]],
+                "elective_cc_count" = cut_off_elec_CC[[1]], "elective_cc_prop" = cut_off_elec_CC[[2]])
+
+write.xlsx(df_list, file = here::here("los_distribution", "totalLoS_episodes_2015-2020.xlsx"))
+
+
+
+
+
