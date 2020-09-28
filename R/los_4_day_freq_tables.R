bc989583ff24f281c66e89672ea92034d137fbb4
fiona-grimm <Fiona.grimm@health.org.uk>
2020-09-28 15:38:39 +0100

    Move 4d los analysis to main script, rename to prevent overwriting intial los analysis, add condition to remove NAs, fix make_prop function call

--- R/los_4_day_freq_tables.R
+++ R/los_4_day_freq_tables.R
@@ -3,10 +3,6 @@
 ##########################################################
 
 ##############################################################################################################
-# Required packages
-require(data.table)
-require(dplyr)
-require(openxlsx)
 
 ## Functions ##
 
@@ -77,83 +73,3 @@ count_threshold <- function(aggregated_list, cut_off = 10){
   return(list(count = count_data, prop = prop_data))
   
 }
-
-##############################################################################################################
-## Load HES data
-system.time(transitions_data <- fread("D:/Overflows/data/HES_APC_CC_0913_transitions_all_ICD.csv"))
-
-# Take only 1 year of data (11-12 in this case)
-
-hes_narrow <- select(transitions_data  , c("totalLoS_cln","GA_LoS", "cc_LoS", "admimeth_C", "ICD",
-                               "agegrp_v3", "cc_start_flg","cohort","cc"))
-hes_narrow$disease <- "ICD"
-
-hes_narrow$age <- paste("_AGE", hes_narrow$agegrp_v3, sep = "")
-
-hes_narrow$ptgrp <- paste0(hes_narrow$disease, hes_narrow$ICD, hes_narrow$age)
-hes_narrow$a <- ifelse(hes_narrow$admimeth_C == 1, "N", "E")
-
-# Identify patients who have GA stay and those with CC stay 
-N_ga <- subset(hes_narrow, admimeth_C == 1 )
-E_ga <- subset(hes_narrow, admimeth_C == 2 )
-
-N_cc <- subset(hes_narrow, admimeth_C == 1 & cc == 1)
-E_cc <- subset(hes_narrow, admimeth_C == 2 & cc == 1)
-
-## subset the GA & CC data for first 4 days only
-N_ga <- subset(N_ga, GA_LoS >= 0 & GA_LoS <= 4)
-E_ga <- subset(E_ga, GA_LoS >= 0 & GA_LoS <= 4)
-
-N_cc <- subset(N_cc, cc_LoS >= 0 & cc_LoS <= 4)
-E_cc <- subset(E_cc, cc_LoS >= 0 & cc_LoS <= 4)
-
-
-# frequency and proportion tables for each category
-# All electives
-
-# Elective patients  G&A
-count_N_ga <- as.data.frame.matrix(table(N_ga$GA_LoS, N_ga$ptgrp))
-aggregated_dfs_elec_GA <- aggregate_weekly(count_N_ga)
-
-# Emergency patients G&A
-count_E_ga <- as.data.frame.matrix(table(E_ga$GA_LoS, E_ga$ptgrp))
-aggregated_dfs_emerg_GA <- aggregate_weekly(count_E_ga)
-
-# Elective patients CC
-count_N_cc <- as.data.frame.matrix(table(N_cc$cc_LoS, N_cc$ptgrp))
-aggregated_dfs_elec_CC <- aggregate_weekly(count_N_cc)
-
-# Emergency patients CC
-count_E_cc <- as.data.frame.matrix(table(E_cc$cc_LoS, E_cc$ptgrp))
-aggregated_dfs_emerg_CC <- aggregate_weekly(count_E_cc)
-
-
-
-###############################################################################
-## Simplify counts to remove those under 10 ###################################
-###############################################################################
-
-## Electives starting in G&A 
-cut_off_elec_GA <- count_threshold(aggregated_dfs_elec_GA, cut_off = 10)
-
-## Emergencies starting in G&A
-cut_off_emerg_GA <- count_threshold(aggregated_dfs_emerg_GA, cut_off = 10)
-
-## Electives starting in CC
-cut_off_elec_CC <- count_threshold(aggregated_dfs_elec_CC, cut_off = 10)
-
-## Emergencies starting in CC
-cut_off_emerg_CC <- count_threshold(aggregated_dfs_emerg_CC, cut_off = 10)
-
-
-df_list <- list("emergency_ga_count" = cut_off_emerg_GA[[1]] ,"emergency_ga_prop" = cut_off_emerg_GA[[2]],
-                "emergency_cc_count" = cut_off_emerg_CC[[1]], "emergency_cc_prop" = cut_off_emerg_CC[[2]],
-                "elective_ga_count" = cut_off_elec_GA[[1]], "elective_ga_prop" = cut_off_elec_GA[[2]],
-                "elective_cc_count" = cut_off_elec_CC[[1]], "elective_cc_prop" = cut_off_elec_CC[[2]])
-
-write.xlsx(df_list, file = "D:/Dropbox/COVID19/Overflow/Distributions/totalLoS_episodes_17_09_2020.xlsx")
-
-
-
-
-
